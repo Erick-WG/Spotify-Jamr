f1aeb5e21b1e696df433bbaf64b603b6a6fcc744
Erick-WG <erickgitahi95@gmail.com>
2025-12-16 19:25:46 +0300

    Add absolute import support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+      '@pages': path.resolve(__dirname, '/src/pages'),
+      '@components': path.resolve(__dirname, '/src/components'),
+      '@assets': path.resolve(__dirname, '/src/assets'),
+      '@css': path.resolve(__dirname, '/src/assets/css'),
+    },
+  },
   plugins: [
     react({
       babel: {
